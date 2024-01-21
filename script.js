3ef14cba05bd989b018d3d04222e013360e4e06c
guigbd <157081603+guigbd@users.noreply.github.com>
2024-01-20 22:37:22 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+const html = document.documentElement
+html.classList.toggle('light')
+// pegar tag img
+const img = document.querySelector("#profile img")
+// substituir a imagem 
+if (html.classList.contains("light")) {
+// se tiver light mode, adicionar a imagem light
+img.setAttribute("src", "./assets/avatar-light.png")
+}else {
+//se tiver sem light mode, manter imagem a normal 
+img.setAttribute("src", "./assets/avatar.png")
+}
+}
